97f3907471a07148750045e58ae979aa7b0c74b7
Hamdrive <hamza.husein017@gmail.com>
2022-05-14 23:03:27 +0530

    [udpate] setup static pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,6 @@
+import { Dashboard } from "./dashboard/Dashboard";
+import { Auth } from "./auth/Auth";
+import { SignUp } from "./signUp/SignUp";
+import { LogIn } from "./logIn/LogIn";
+
+export { Dashboard, Auth, SignUp, LogIn };
---
+++ src/pages/logIn/LogIn.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LogIn = () => {
+  return (
+    <div>LogIn</div>
+  )
+}
