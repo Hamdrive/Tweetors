cbdca84a2325cc1904a4d375d38acddec1727ec6
Hamdrive <hamza.husein017@gmail.com>
2022-05-16 23:11:38 +0530

    [update] layout of tabs

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -10,8 +10,6 @@ export const Dashboard = () => {
 
   const { dataState, getExistingTweetors } = useData();
 
-  console.log(dataState, getExistingTweetors);
-
   useEffect(() => {
     getExistingTweetors();
   }, []);
@@ -36,34 +34,29 @@ export const Dashboard = () => {
                   tabIndex === 0 && "selectedTab"
                 } `}
               >
-                Explore
+                Your Tweetors
               </Tab>
               <Tab
                 className={`w-50 txt-center h4 txt-semibold py-sm pointer ${
                   tabIndex === 1 && "selectedTab"
                 } `}
               >
-                Your Tweetors
+                Tweets
               </Tab>
             </TabList>
 
-            <TabPanel className={"ov-y-scroll tabPanel"}>
-              {dataState.tweetContentIds.map((id) =>
-                console.log(id)
-                // <TwitterTweetEmbed tweetId={id} />
-              )}
-              {/* <TwitterTweetEmbed tweetId={"1525378942554624000"} />
-              <TwitterTweetEmbed tweetId={"1525808717324861441"} />
-              <TwitterTweetEmbed tweetId={"1524445508407726081"} />
-              <TwitterTweetEmbed tweetId={"1525779769983242240"} /> */}
-              <ComponentLoader />
-            </TabPanel>
             <TabPanel className={"ov-y-scroll tabPanel"}>
               <TwitterTweetEmbed tweetId={"1525388160896643073"} />
               <TwitterTweetEmbed tweetId={"1525429448103067648"} />
               <TwitterTweetEmbed tweetId={"1525561780319027200"} />
               <TwitterTweetEmbed tweetId={"1524807470845071368"} />
               <TwitterTweetEmbed tweetId={"1523723819936284672"} />
+              <ComponentLoader />
+            </TabPanel>
+            <TabPanel className={"ov-y-scroll tabPanel"}>
+              {dataState.tweetContentIds.map((id, index) => (
+                <TwitterTweetEmbed key={index} hide_thread={true} tweetId={id} />
+              ))}
               <ComponentLoader />
             </TabPanel>
           </Tabs>
