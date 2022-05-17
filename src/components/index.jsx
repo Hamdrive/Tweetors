d9c4b5e628286a255e30a6f35ed1558b39fb6e5c
Hamdrive <hamza.husein017@gmail.com>
2022-05-17 21:29:50 +0530

    [feat] search component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,7 @@
 import { Loader } from "./loader/Loader";
 import { InputError } from "./inputError/InputError";
 import { ComponentLoader } from "./componentLoader/ComponentLoader";
+import { Search } from "./search/Search";
+import { EmptyResult } from "./emptyResult/EmptyResult";
 
-export { Loader, InputError, ComponentLoader };
+export { Loader, InputError, ComponentLoader, Search, EmptyResult };
