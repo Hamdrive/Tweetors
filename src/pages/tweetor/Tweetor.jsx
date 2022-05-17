1c1a8423ecad9fef89353719c0577663ae66c25d
Hamdrive <hamza.husein017@gmail.com>
2022-05-17 21:28:38 +0530

    [update] handle new/delete tweetor

--- src/pages/tweetor/Tweetor.jsx
+++ src/pages/tweetor/Tweetor.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 
 export const Tweetor = ({ tweetor, handleDelete }) => {
-  const { name, userName, pfpUrl } = tweetor;
+  const { name, username, profile_image_url } = tweetor;
 
   return (
     <div className="tweetor__main">
-      <section className="flex-around tweetor__section">
-        <div className="tweetor__pfp">
-          <img src={pfpUrl} alt="Tweetor pfp" />
+      <section className="flex-around tweetor__section tweetor__grid">
+        <div className="tweetor__pfp mx-auto">
+          <img src={profile_image_url} alt="Tweetor pfp" />
         </div>
         <p className="h4 txt-regular tweetor__name">{name}</p>
         <a
-          href={`https://twitter.com/${userName}`}
+          href={`https://twitter.com/${username}`}
           target="_blank"
           rel="noreferrer"
-          title={`Visit ${userName}'s Twitter Profile`}
+          title={`Visit ${username}'s Twitter Profile`}
         >
           <div className="fab fa-twitter profile__btn"></div>
         </a>
         <div
-          onClick={() => handleDelete(userName)}
-          title={`Delete ${userName}`}
+          onClick={() => handleDelete(username)}
+          title={`Delete ${username}`}
           className="fas fa-trash trash__btn pointer"
         ></div>
       </section>
