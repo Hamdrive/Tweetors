ceb324e593a683ceba2c0ac3266a79e42693fb23
Hamdrive <hamza.husein017@gmail.com>
2022-05-20 22:19:07 +0530

    [feat] custom tweet card

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,5 +4,6 @@ import { ComponentLoader } from "./componentLoader/ComponentLoader";
 import { Search } from "./search/Search";
 import { EmptyResult } from "./emptyResult/EmptyResult";
 import { Footer } from "./footer/Footer";
+import { TweetCard } from "./tweetCard/TweetCard";
 
-export { Loader, InputError, ComponentLoader, Search, EmptyResult, Footer };
+export { Loader, InputError, ComponentLoader, Search, EmptyResult, Footer, TweetCard };
