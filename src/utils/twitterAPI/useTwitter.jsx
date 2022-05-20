6370f90c7edacd42ffbb77c532b28b0584677add
Hamdrive <hamza.husein017@gmail.com>
2022-05-20 10:46:12 +0530

    [update] handle serverless and manifest

--- src/utils/twitterAPI/useTwitter.jsx
+++ src/utils/twitterAPI/useTwitter.jsx
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const fetchTwitterUser = async (username) => {
   try {
-    const res = await axios.get(`/api/getTwitterUser?username=${username}`);
+    const res = await axios.get(
+      `https://twitter-mentors.vercel.app/api/getTwitterUser?username=${username}`
+    );
 
     return res;
   } catch (error) {
@@ -12,7 +14,9 @@ export const fetchTwitterUser = async (username) => {
 
 export const fetchRecentTweet = async (userId, dispatch) => {
   try {
-    const res = await axios.get(`/api/getTwitterUserTweets?userID=${userId}`);
+    const res = await axios.get(
+      `https://twitter-mentors.vercel.app/api/getTwitterUserTweets?userID=${userId}`
+    );
 
     if (res?.data?.data.hasOwnProperty("referenced_tweets")) return;
     else dispatch({ type: "ADD_TWEET_ID", payload: res?.data?.data[0]?.id });
